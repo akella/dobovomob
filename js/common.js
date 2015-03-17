9413ec218b9d086859f175f5cfc0bcb9cf2d166e
Валерий <kaktuz34@gmail.com>
2015-03-17 20:11:03 +0200

    catalogue_filters page

--- js/common.js
+++ js/common.js
@@ -49,7 +49,30 @@ head.ready(function() {
 			var input = $(this).find("input");
 			var plus = $(this).find(".js-plus");
 			var minus = $(this).find(".js-minus");
+			var plus_guests = $(this).find(".js-choose-guests .js-plus");
+			var minus_guests = $(this).find(".js-choose-guests .js-minus");
 			plus.bind("click", function(){
+				var val = +(input.val());
+				if (val >= max_number) {
+					return false
+				}
+				else {
+					val += 1;
+					input.val(val);
+				}
+			});
+			minus.bind("click", function(){
+				var val = +(input.val());
+				if (val > 1) {
+					val -= 1;
+					input.val(val);
+				}
+				else {
+					input.val('');
+					return false
+				}
+			});
+			plus_guests.bind("click", function(){
 				var val = +(input.val());
 				if (val >= max_number) {
 					return false
@@ -60,7 +83,7 @@ head.ready(function() {
 					$('.js-guests').text("гостей");
 				}
 			});
-			minus.bind("click", function(){
+			minus_guests.bind("click", function(){
 				var val = +(input.val());
 				if (val > 1) {
 					val -= 1;
@@ -76,34 +99,96 @@ head.ready(function() {
 		});
 	}
 	choose();
-
+	
 	// datepicker
-	$("#from").change(function() {
-		var date = $(this).datepicker("getDate");
-		$("#d1").text(date);	
+	$('#from').on('click', function(){
+		$(this).parent().parent().parent().addClass('is-active');
+		var array = ["2015-03-18","2015-03-19","2015-03-20"]
+		$( "#from" ).datepicker({
+			 beforeShowDay: function(date){
+		        var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+		        return [ array.indexOf(string) == -1 ]
+		    },
+			inline: true,
+			dateFormat: 'd MM D',
+			monthNamesShort: ['янв.', 'фев.', 'март.', 'апр.', 'май.', 'июнь.', 'июль.', 'авг.', 'сент.', 'окт.', 'ноя.', 'дек.'],
+			monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+			dayNamesShort: ['вс','пн','вт','ср', 'чт', 'пт','сб'],
+			dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+			firstDay: 1,
+			minDate: 0,
+			onSelect: function( selectedDate ) {
+				$( "#to" ).datepicker({
+					inline: true,
+					minDate: selectedDate,
+					dateFormat: 'd MM D',
+					monthNamesShort: ['янв.', 'фев.', 'март.', 'апр.', 'май.', 'июнь.', 'июль.', 'авг.', 'сент.', 'окт.', 'ноя.', 'дек.'],
+					monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+					dayNamesShort: ['вс','пн','вт','ср','чт','пт','сб'],
+					dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+					firstDay: 1,
+					onSelect: function( selectedDate ) {
+						$('#to').removeClass('hasDatepicker');
+						$('#to .ui-datepicker').hide();
+						$(".datepicker__to-text").text(selectedDate);
+						var OurString = $(".datepicker__to-text").html();
+						var NewOurString = "";
+							OurString = OurString.split(" ");
+							for ( i = 0; i < OurString.length; i++ ) {
+							NewOurString = NewOurString + "<span>" + OurString[i] + "</span>";
+						}
+						$(".datepicker__to-text").html(NewOurString);
+						$(this).parent().parent().parent().removeClass('is-active');
+					}
+				});
+				$('#from').removeClass('hasDatepicker');
+				$('#from .ui-datepicker').hide();
+				$(".datepicker__from-text").text(selectedDate);
+				var OurString = $(".datepicker__from-text").html();
+				var NewOurString = "";
+					OurString = OurString.split(" ");
+					for ( i = 0; i < OurString.length; i++ ) {
+					NewOurString = NewOurString + "<span>" + OurString[i] + "</span>";
+				}
+				$(".datepicker__from-text").html(NewOurString);
+			}
+		});
 	});
-	$( "#from" ).datepicker({
-		inline: true,
-		dateFormat: 'MM d',
-		monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-		monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-		firstDay: 1,
-		onClose: function( selectedDate ) {
-			$( "#to" ).datepicker( "option", "minDate", selectedDate );
+	$('#to').on('click', function(){
+		$( "#to" ).datepicker({
+			inline: true,
+			dateFormat: 'd MM D',
+			monthNamesShort: ['янв.', 'фев.', 'март.', 'апр.', 'май.', 'июнь.', 'июль.', 'авг.', 'сент.', 'окт.', 'ноя.', 'дек.'],
+			monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+			dayNamesShort: ['вс','пн','вт','ср','чт','пт','сб'],
+			dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+			firstDay: 1,
+			minDate: 0,
+			onSelect: function( selectedDate ) {
+				$('#to').removeClass('hasDatepicker');
+				$('#to .ui-datepicker').hide();
+				$(".datepicker__to-text").text(selectedDate);
+				var OurString = $(".datepicker__to-text").html();
+				var NewOurString = "";
+					OurString = OurString.split(" ");
+					for ( i = 0; i < OurString.length; i++ ) {
+					NewOurString = NewOurString + "<span>" + OurString[i] + "</span>";
+				}
+				$(".datepicker__to-text").html(NewOurString);
+				$(this).parent().parent().parent().removeClass('is-active');
+			}
+		});
+		$(".datepicker__from-text").text('--');
+	});
+	
+	// cost
+	$('.js-cost-input').on('input', function (){
+		if ( $(this).val().length) {
+			$('.js-cost-checkbox').attr('disabled', 'disabled');
+		}
+		else {
+			$('.js-cost-checkbox').removeAttr('disabled');
 		}
 	});
-	$( "#to" ).datepicker({
-		inline: true,
-		dateFormat: 'MM d',
-		monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-		monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-		firstDay: 1,
-		onClose: function( selectedDate ) {
-			$( "#from" ).datepicker( "option", "maxDate", selectedDate );
-		}
-	});
-
 
 });
