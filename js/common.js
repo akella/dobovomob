a820104265441e70b3323d3a30820975ee84657f
Валерий <kaktuz34@gmail.com>
2015-03-19 17:53:01 +0200

    apartments_card_01 apartments_card_01_on_demand pages

--- js/common.js
+++ js/common.js
@@ -35,6 +35,26 @@ head.ready(function() {
 		slidesToShow: 1,
 		slidesToScroll: 1
 	});
+	$('.slider-auto').on('init', function(slick){
+		var totalReviews = $('.slider-auto .slick-slide').length;
+		$('.js-number-total').text(totalReviews);
+		$('.js-number-current').text('1');
+	});
+	$('.slider-auto').slick({
+		arrows: false,
+		dots: false,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		adaptiveHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+	$('.slider-auto').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		nextSlide++;
+		$('.js-number-current').text(nextSlide);
+	});
 
 	// filter open
 	$('.js-open').on('click', function() {
@@ -194,7 +214,6 @@ head.ready(function() {
 	// gmap
 	function initialize() {
 		var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-		var myLatlng_2 = new google.maps.LatLng(-25.303882,131.034922);
 		var mapOptions = {
 			zoom: 10,
 			center: myLatlng
@@ -234,7 +253,7 @@ head.ready(function() {
 
 		var infowindow = new google.maps.InfoWindow({
 			content: contentString,
-			maxWidth: 0
+			maxWidth: 1
 		});
 		var image = new google.maps.MarkerImage('img/marker2x.png', null, null, null, new google.maps.Size(20,30));
 		var marker = new google.maps.Marker({
@@ -250,5 +269,89 @@ head.ready(function() {
 	}
 	google.maps.event.addDomListener(window, 'load', initialize);
 
+	// gmap-card
+	function initialize_card_map() {
+		var myLatlng_card = new google.maps.LatLng(-25.303882,131.034922);
+		var mapOptions = {
+			zoom: 10,
+			center: myLatlng_card
+		};
+
+		var map_card = new google.maps.Map(document.getElementById('map-card'), mapOptions);
+		var contentString_card = '<div class="popup-map">'+
+				'<div class="popup-map__top">'+
+					'<div class="popup-map__img">'+
+						'<img src="img/img-map.png" alt="">'+
+						'<div class="price-black">'+
+							'<strong class="price-black__top">9.1</strong>'+
+							'<span class="price-black__bott">12 отзывов</span>'+
+						'</div>'+
+					'</div>'+
+					'<div class="popup-map__right">'+
+						'<a href="#" class="popup-map__title">Двухуровневая квартира в самом центре столицы</a>'+
+						'<div class="popup-map__id">(AV 2478)</div>'+
+						'<div class="popup-map__address">Двухкомнатная</div>'+
+						'<div class="popup-map__row">'+
+							'<div class="popup-map__col">'+
+								'<div class="popup-map__price popup-map__price_green"><span>320</span>грн. в сутки</div>'+
+								'<p>без предоплаты</p>'+
+							'</div>'+
+							'<div class="popup-map__col">'+
+								'<div class="popup-map__price"><span>960</span>грн.</div>'+
+								'<p>за 3 ночи</p>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+				'<div class="popup-map__bottom">'+
+					'<button class="btn btn_green-arr">Забронировать</button>'+
+				'</div>'+
+			'</div>';
+
+		var infowindow_card = new google.maps.InfoWindow({
+			content: contentString_card,
+			maxWidth: 1
+		});
+
+		var image_card = new google.maps.MarkerImage('img/marker2x2.png', null, null, null, new google.maps.Size(35,50));
+		var marker_card = new google.maps.Marker({
+			position: myLatlng_card,
+			map: map_card,
+			icon: image_card,
+			title: 'Двухуровневая квартира в самом центре столицы'
+		});
+
+		google.maps.event.addListener(marker_card, 'click', function() {
+			infowindow_card.open(map_card,marker_card);
+		});
+	}
+	google.maps.event.addDomListener(window, 'load', initialize_card_map);
+
+	// description
+	$('.js-description-btn').on("click", function () {
+		$('.js-description').toggleClass('is-active');
+		return false;
+	});
+
+	// discounts open block
+	$('.js-open-block').on('click', function (){
+		var this_par = $(this).parent();
+		var text = $('.js-open-block span');
+		if (this_par.hasClass('is-active')) {
+			this_par.removeClass('is-active');
+			text.text('все акции и скидки');
+		}
+		else {
+			this_par.addClass('is-active');
+			text.text('скрыть');
+		}
+		return false;
+	});
+
+	// tabs
+	$( "#tabs" ).tabs();
+	$('#tabs li a').on('click', function (){
+		$('.slick-slider').slick('reinit');
+	});
 
 });
